104d9380b9b3bb046f85d7a4e53af5751db3ce28
Dominik Stodolny <dom@dmk.st>
2017-03-02 11:23:34 +0000

    put builds array objects in one line

--- test/index.js
+++ test/index.js
@@ -4,15 +4,9 @@ const getStats = require('../index').getStats;
 describe('getStats', () => {
   it('should return a collection of builds', (done) => {
     const builds = [
-      {
-        'start_time': '2017-03-02T10:18:33.094Z',
-      },
-      {
-        'start_time': '2017-03-02T10:18:33.094Z',
-      },
-      {
-        'start_time': '2017-02-02T10:18:33.094Z',
-      },
+      { 'start_time': '2017-03-02T10:18:33.094Z' },
+      { 'start_time': '2017-03-02T10:18:33.094Z' },
+      { 'start_time': '2017-02-02T10:18:33.094Z' },
     ];
     const fetchBatch = (offset, batchSize) => new Promise(resolve => resolve(builds))
     getStats(fetchBatch)
