90fd0f46bc9401f82e18d7cdf2f55991537254c8
Dominik Stodolny <dom@dmk.st>
2017-03-03 10:39:25 +0000

    send report to slack

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 const request = require('request');
 const getStats = require('./getStats');
 
+const circleCItoken = process.env.CIRCLECI_TOKEN;
+const slackEndpoint = process.env.SLACK_ENDPOINT;
+
 const circleCIFetchBatch = (offset) => {
-  const token = process.env.CIRCLECI_TOKEN;
   const endpoint = 'https://circleci.com/api/v1.1/project/github/redbadger/website-honestly';
   const batchSize = 30;
   const options = {
@@ -12,7 +14,7 @@ const circleCIFetchBatch = (offset) => {
     uri: endpoint,
     json: true,
     qs: {
-      'circle-token': token,
+      'circle-token': circleCItoken,
       limit: batchSize,
       offset: offset,
     }
@@ -24,7 +26,27 @@ const circleCIFetchBatch = (offset) => {
   })
 }
 
+const sendToSlack = stats => {
+  const report = `
+Failed builds: ${stats.failedBuildsPercentage}%
+Code deployments: ${stats.codeDeploymentCount}
+Average build time: ${stats.averageBuildTime}ms
+`
+  const options = {
+    method: 'POST',
+    uri: slackEndpoint,
+    json: {
+      'text': report
+    }
+  }
+  request(options);
+}
+
 exports.handler = (event, context, callback) => {
   getStats(circleCIFetchBatch, new Date('2017-03-01'))
     .then(stats => { console.log(stats) });
 }
+
+
+getStats(circleCIFetchBatch, new Date('2017-02-01'))
+  .then(sendToSlack);
